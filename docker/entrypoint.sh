59e005b240772926afdba13f429d5b5ec240633f
baev_kirill <kbaiev@provectus.com>
2020-05-06 01:22:41 +0300

    add repository for library

---
+++ docker/entrypoint.sh
@@ -0,0 +1,4 @@
+
+curl -O https://github.com/baevkir/library-project/raw/mvn-repo/com/familymoney/family-money/0.0.1-SNAPSHOT/family-money-0.0.1-20200505.210058-1.jar
+
+java -jar /family-money-bot/target/telegrambot-0.0.1-SNAPSHOT.jar
