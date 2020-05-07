9e1b24c8cada586f4afb60fbc04e0d36d7a921d3
baev_kirill <kbaiev@provectus.com>
2020-05-07 13:33:55 +0300

    update application properties file

--- docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-
-curl -O https://github.com/baevkir/library-project/raw/mvn-repo/com/familymoney/family-money/0.0.1-SNAPSHOT/family-money-0.0.1-20200505.210058-1.jar
-
-java -jar /family-money-bot/target/telegrambot-0.0.1-SNAPSHOT.jar
